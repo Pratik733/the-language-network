0950569c250d56d45d7174177bb90786a98846e2
Pratik733 <pratik.tayade733@gmail.com>
2022-02-19 22:55:40 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react'
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Curriculum from './components/curriculum/Curriculum';
 import KidsCourses from './components/KidsCourses/KidsCourses';
-import IntroCourse from './components/Footer/LangForKids/IntroCourse';
+import IntroCourse from './components/Footer/LangForAdults/IntroCourse';
+import Earth from './components/Earth/Earth';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
           {/* <Route exact path='/curriculum' element={<Curriculum />} /> */}
           <Route exact path='/KidsCourses' element={<KidsCourses />} />
           <Route exact path='/IntoCourses' element={<IntroCourse />} />
+          <Route exact path='/Earth' element={<Earth />} />
         </Routes>
         <Footer />
       </Router>
