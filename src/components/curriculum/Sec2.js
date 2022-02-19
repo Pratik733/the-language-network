8f7736a562e8f4180dd35f3fe375b0dbf3620b6b
Pratik733 <pratik.tayade733@gmail.com>
2022-02-20 00:39:15 +0530

    url change

--- src/components/curriculum/Sec2.js
+++ src/components/curriculum/Sec2.js
@@ -18,19 +18,31 @@ export default function Sec2() {
         }
     };
 
+    const change = (url) => {
+        const nextURL = url;
+        const nextTitle = 'adol';
+        const nextState = { additionalInformation: 'Updated the URL with JS' };
+
+        // This will create a new entry in the browser's history, without reloading
+        window.history.pushState(nextState, nextTitle, nextURL);
+
+        // This will replace the current entry in the browser's history, without reloading
+        window.history.replaceState(nextState, nextTitle, nextURL)
+
+    }
 
 
     return (
         <div>
             <div className='w-full flex justify-center'>
-                <a onClick={() => setLvl(1)} id={Lvl===1 ? 'btnP' : ''} class="buttons bg-white py-2.5 px-4 rounded-lg shadow mx-3">
-                Adults Curriculum
+                <a onClick={() => {change("Adults-Curriculum");setLvl(1);}} id={Lvl === 1 ? 'btnP' : ''} class="buttons bg-white py-2.5 px-4 rounded-lg shadow mx-3">
+                    Adults Curriculum
                 </a>
-                <a onClick={() => setLvl(2)} id={Lvl===2 ? 'btnP' : ''} class="buttons bg-white  py-2.5 px-4 rounded-lg shadow mx-3">
-                Adolescents Curriculum
+                <a onClick={() => {change("Adolescents-Curriculum");setLvl(2);}} id={Lvl === 2 ? 'btnP' : ''} class="buttons bg-white  py-2.5 px-4 rounded-lg shadow mx-3">
+                    Adolescents Curriculum
                 </a>
-                <a onClick={() => setLvl(3)} id={Lvl===3 ? 'btnP' : ''} class="buttons bg-white py-2.5 px-4 rounded-lg shadow mx-3">
-                Kids Curriculum 
+                <a onClick={() => {change("Kids-Curriculum");setLvl(3);}} id={Lvl === 3 ? 'btnP' : ''} class="buttons bg-white py-2.5 px-4 rounded-lg shadow mx-3">
+                    Kids Curriculum
                 </a>
             </div>
             <Age />
